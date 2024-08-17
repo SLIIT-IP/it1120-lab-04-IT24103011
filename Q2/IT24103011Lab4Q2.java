5a4c3368b06bf1e58754eb45e905415be53d5a21
IT24103011 <it24103011@my.sliit.lk>
2024-08-17 23:24:50 +0530

    IT24103011Lab4Q2.java

--- Q2/IT24103011Lab4Q2.java
+++ Q2/IT24103011Lab4Q2.java
@@ -1,46 +1,34 @@
 import java.util.Scanner;
-
-public class IT24103011Lab4Q2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        // Input exam marks
-        int exMark;
-        do {
-            System.out.print("Please enter exam marks (out of 100): ");
-            exMark = input.nextInt();
-            if (exMark < 0 || exMark > 100) {
-                System.out.println("Invalid input for exam marks. Marks should be between 0 and 100.");
-            }
-        } while (exMark < 0 || exMark > 100);
-
-        // Input lab submission marks
-        int labMark;
-        do {
-            System.out.print("Please enter lab submission marks (out of 100): ");
-            labMark = input.nextInt();
-            if (labMark < 0 || labMark > 100) {
-                System.out.println("Invalid input for lab submission marks. Marks should be between 0 and 100.");
-            }
-        } while (labMark < 0 || labMark > 100);
-
-        // Input percentages for exam and lab submission
-        int exPecntge;
-        int labPecntge;
-        do {
-            System.out.print("Please enter the percentage given for exam: ");
-            exPecntge = input.nextInt();
-            System.out.print("Please enter the percentage given for lab submission: ");
-            labPecntge = input.nextInt();
-            if (exPecntge + labPecntge != 100) {
-                System.out.println("The percentages must add up to 100. Please try again.");
-            }
-        } while (exPecntge + labPecntge != 100);
-
-        // Calculate final marks
-        double finalMarks = (exMark * exPecntge / 100.0) + (labMark * labPecntge / 100.0);
-
-        // Display final marks
-        System.out.printf("Final marks: %.2f\n", finalMarks);
-    }
+public class IT24103011Lab4Q2{
+	public static void main (String [] args ){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please Enter exam marks out of 100:  ");
+		int a= sc.nextInt();
+		if (a< 0 || a > 100)
+		{
+		System.out.print("Invalid input for exam marks. Terminating process");
+		}
+		else if (a >= 0 || a <= 100){
+			System.out.print("Please enter labsubmission marks out of 100:  ");
+			int b = sc.nextInt();
+			
+				if (b< 0 || b> 100){
+					System.out.print("Invalid input for exam marks. Terminating program");
+				}
+				else if (a >= 0 || a <= 100){
+					System.out.print("Enter exam percentage:  ");
+					int c = sc.nextInt ();
+					System.out.print("Enter lab submission percentage:  ");	
+					int d = sc.nextInt ();
+						
+						if ((c+d)>100){
+							System.out.print ("The percentages must add up to 100 Terminating program");
+						}
+						else if ((c+d)<=100){
+							float final_marks= ((a*c)/100)+((b*d)/100);
+							System.out.print ("Final Exam Marks : "+final_marks );
+						}
+				}
+		}
+	}
 }
